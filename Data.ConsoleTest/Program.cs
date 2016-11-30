29f3642795b9c65d7b3b37846a72de44b5e0174a
Aleksander <scorpinoc@bigmir.net>
2016-11-30 15:09:49 +0200

    ConsoleTest refactoring

--- Data.ConsoleTest/Program.cs
+++ Data.ConsoleTest/Program.cs
@@ -13,71 +13,74 @@ namespace Data.ConsoleTest
     {
         private static void Main()
         {
-            int testIndex;
-            WriteLine("enter test number 0 to 4");
-            if (!int.TryParse(ReadLine(), out testIndex))
-                return;
-            switch (testIndex)
+            while (true)
             {
-                case 0:
-                    FullTestColnists(new DataContainer(new Position(1, 1)), "basic MoveTest use", BasicTest);
-                    break;
-                case 1:
-                    FullTestColnists(new DataContainer(new Position(1, 1)), "child", ChildTest);
-                    break;
-                case 2:
-                    FullTestColnists(new DataContainer(new Position(100, 100)), "Random MovingModuleJob", RandomModuleMove);
-                    break;
-                case 3:
-                    FullTestColnists(new DataContainer(new Position(100, 100)), "SingleTargeted MovingModuleJob - to {{50;50}}", SignleTargetModuleMoveTestInitializer);
-                    break;
-                case 4:
-                    FullTestColnists(new DataContainer(new Position(100, 100)),
-                        "MultiTargeted MovingModuleJob - to {{25;25}, {25;75}, {75;75}, {75;25}}", MultiTargetModuleMoveTestInitializer);
-                    break;
-                default:
-                    WriteLine("No such test number");
-                    break;
-            }
+                var tests = new Action[]
+                {
+                    BasicTest,
+                    ChildTest,
+                    RandomModuleMoveTest,
+                    SignleTargetModuleMoveTest,
+                    MultiTargetModuleMoveTest,
+                };
 
-            ReadKey(true);
+                WriteLine($"enter test number 0 to {tests.Length - 1} (anithing else to exit)");
+                int testIndex;
+                if (!int.TryParse(ReadLine(), out testIndex)) return;
+                if (testIndex > 0 && testIndex < tests.Length)
+                    tests[testIndex]();
+                else
+                {
+                    WriteLine("No such test number - exit");
+                    return;
+                }
+            }
         }
 
 
         #region Tests
 
-        private static void MultiTargetModuleMoveTestInitializer(DataContainer obj)
+        private static void MultiTargetModuleMoveTest()
         {
+            var obj = new DataContainer(new Position(100, 100));
             var c = 'A';
-            var job = new MovingModuleJob("Targered Position Moving", new MoveAction(new MultiTargetPositionAction(obj,
+            var job = new MovingModuleJob("test",
+                new MoveAction(new MultiTargetPositionAction(obj,
                 data => new[] { new Position(25, 25), new Position(25, 75), new Position(75, 75), new Position(75, 25), })));
             obj.Add(job);
             var rand = new Random();
-            for (var i = 0; i < 3; ++i)
-                obj.Add(new Colonist((c++).ToString(), new Position((uint)rand.Next(25, 75), (uint)rand.Next(25, 75)), job));
+            for (var i = 0; i < 4; ++i)
+                obj.Add(new Colonist((c++).ToString(), new Position((uint)rand.Next(20, 80), (uint)rand.Next(20, 80)), job));
+            Test(obj.Colonists, $"{nameof(MovingModuleJob)} -> {nameof(MultiTargetPositionAction)} to {{{{25;25}}, {{25;75}}, {{75;75}}, {{75;25}}}}");
         }
 
-        private static void SignleTargetModuleMoveTestInitializer(DataContainer obj)
+        private static void SignleTargetModuleMoveTest()
         {
+            var obj = new DataContainer(new Position(100, 100));
             var c = 'A';
-            var job = new MovingModuleJob("Targered Position Moving", new MoveAction(new SingleTargetPositionAction(obj, data => new Position(50, 50))));
+            var job = new MovingModuleJob("test",
+                new MoveAction(new SingleTargetPositionAction(obj, data => new Position(50, 50))));
             obj.Add(job);
             var rand = new Random();
             for (var i = 0; i < 3; ++i)
                 obj.Add(new Colonist((c++).ToString(), new Position((uint)rand.Next(15, 85), (uint)rand.Next(25, 75)), job));
+            Test(obj.Colonists, $"{nameof(MovingModuleJob)} -> {nameof(SingleTargetPositionAction)} to {{50;50}}");
         }
 
-        private static void RandomModuleMove(DataContainer obj)
+        private static void RandomModuleMoveTest()
         {
+            var obj = new DataContainer(new Position(100, 100));
             var c = 'A';
-            var job = new MovingModuleJob("Random Position Moving", new MoveAction(/*5,*/ new RandomPositionAction(obj)));
+            var job = new MovingModuleJob("test", new MoveAction(/*5,*/ new RandomPositionAction(obj)));
             obj.Add(job);
             for (var i = 0; i < 5; ++i)
                 obj.Add(new Colonist((c++).ToString(), new Position(10, 10), job));
+            Test(obj.Colonists, $"{nameof(MovingModuleJob)} -> {nameof(RandomPositionAction)}");
         }
 
-        private static void ChildTest(DataContainer data)
+        private static void ChildTest()
         {
+            var data = new DataContainer(new Position(1, 1));
             var c = 'A';
             var a = new Colonist((c++).ToString(), new Position(10, 10), new MoveTest());
             var b = new Colonist((c).ToString(), new Position(10, 10), new MoveTest());
@@ -85,24 +88,19 @@ namespace Data.ConsoleTest
             data.Add(a);
             data.Add(b);
             data.Add(child);
+            Test(data.Colonists, $"child generation");
         }
 
-        private static void BasicTest(DataContainer data)
+        private static void BasicTest()
         {
+            var data = new DataContainer(new Position(1, 1));
             var c = 'A';
             for (var i = 0; i < 5; ++i)
                 data.Add(new Colonist((c++).ToString(), new Position(10, 10), new MoveTest()));
+            Test(data.Colonists, $"Basic {nameof(MoveTest)}");
         }
 
         #endregion
-
-        private static void FullTestColnists(DataContainer data, string testName, Action<DataContainer> testInitializer)
-        {
-            if (data == null) return;
-            testInitializer?.Invoke(data);
-            Test(data.Colonists, testName);
-            ReadKey(true);
-        }
 
         private static void Test(IEnumerable<Colonist> testData, string testName)
         {
@@ -112,13 +110,16 @@ namespace Data.ConsoleTest
             var colonists = testData as IList<Colonist> ?? testData.ToList();
             colonists.Print();
 
-            WriteLine("\nmoving");
+            WriteLine();
+            WriteLine("moving");
+
             for (int i = 0, n = new Random().Next(10, 25); i < n; ++i)
             {
                 WriteLine("\tTick {0}", i);
                 colonists.Work();
                 colonists.Print();
             }
+            ReadKey(true);
         }
     }
 }
